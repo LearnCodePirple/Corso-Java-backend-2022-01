7d6e2e8427cf6f622355f9a2e9f14a23e4ce189b
nunu <luigizaniboni2@gmail.com>
2022-03-01 19:04:14 +0100

    Create StringReverse.java

---
+++ module_01/src/StringReverse.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+    public class StringReverse {
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.print("Enter string:");
+            String a = sc.nextLine();
+            stringReverse(a);
+        }
+
+        static void stringReverse(String s) {
+
+        }
+    }
